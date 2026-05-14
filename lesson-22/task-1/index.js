88e2398adf4441bad904490ebc79b14e37102c3b
RostislavT1 <rostislav49538@gmail.com>
2026-05-14 18:36:50 +0200

    task-1

---
+++ lesson-22/task-1/index.js
@@ -0,0 +1,46 @@
+const elem = document.querySelector(".rect_div");
+const p = document.querySelector(".rect_p");
+const span = document.querySelector(".rect_span");
+const evenstListSelector = document.querySelector(".events-list");
+const buttonClearElement = document.querySelector(".clear-btn");
+const buttonRemoveHandlers = document.querySelector(".remove-handlers-btn");
+const buttonAttachHandlers = document.querySelector(".attach-handlers-btn");
+
+function checkEventClick(text, color) {
+    evenstListSelector.innerHTML += `<span style = "color: ${color}; margin-left: 8px">${text}</span>`;
+}
+
+const bindGreenDiv = checkEventClick.bind(null, "DIV", "green");
+const bindGreenP = checkEventClick.bind(null, "P", "green");
+const bindGreenSpan = checkEventClick.bind(null, "DIV", "green");
+const bindGrayDiv = checkEventClick.bind(null, "DIV", "gray");
+const bindGrayP = checkEventClick.bind(null, "P", "gray");
+const bindGraySpan = checkEventClick.bind(null, "DIV", "gray");
+
+function clearContentButton() {
+    evenstListSelector.innerHTML = "";
+}
+
+function removeHandlerButton() {
+    elem.removeEventListener("click", bindGreenDiv);
+    p.removeEventListener("click", bindGreenP);
+    span.removeEventListener("click", bindGreenSpan);
+    elem.removeEventListener("click", bindGrayDiv, true);
+    p.removeEventListener("click", bindGrayP, true);
+    span.removeEventListener("click", bindGraySpan, true);
+}
+
+function attachHanlerButton() {
+    elem.addEventListener("click", bindGreenDiv);
+    p.addEventListener("click", bindGreenP);
+    span.addEventListener("click", bindGreenSpan);
+
+    elem.addEventListener("click", bindGrayDiv, true);
+    p.addEventListener("click", bindGrayP, true);
+    span.addEventListener("click", bindGraySpan, true);
+}
+
+attachHanlerButton();
+buttonClearElement.addEventListener("click", clearContentButton);
+buttonRemoveHandlers.addEventListener("click", removeHandlerButton);
+buttonAttachHandlers.addEventListener("click", attachHanlerButton);
